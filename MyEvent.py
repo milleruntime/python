295d4f22954b41c74ae749fbfb4bb563dea178cc
Mike Miller <mmiller@apache.org>
2017-08-09 16:51:07 -0400

    Improvements

--- MyEvent.py
+++ MyEvent.py
@@ -7,14 +7,16 @@ import random
 class MyEvent:
 	event_types = ['Cosmic','Alien','Apocolyptic', 'Discovery']
 
+	started = False
 	number = 0
 	name = ''
 
 	def __init__(self, num):
 		self.number = random.randint(1,num)
+		self.name = MyEvent.event_types[random.randint(0,len(MyEvent.event_types)-1)]
 		
 	def start(self):
-		self.name = MyEvent.event_types[random.randint(0,len(MyEvent.event_types)-1)]
+		started = True
 		print self.name + " event occured with power " + str(self.number)
 
 	def __str__(self):
@@ -22,3 +24,6 @@ class MyEvent:
 
 	def __repr__(self):
     		return self.__str__()
+
+	def process(self):
+		return self.number
--- mud.py
+++ mud.py
@@ -5,12 +5,18 @@ import random
 import time
 from MyEvent import MyEvent
 
-ore = 0
-bmat = 0
-energy = 0
-bases = []
-ships = []
-events = []
+class Mud:
+	ore = 0
+	bmat = 0
+	energy = 0
+	bases = []
+	ships = []
+	events = []
+
+	@staticmethod
+	def newEvent(maxPower):
+		eve = MyEvent(maxPower)
+		Mud.events.append(eve)
 
 def check_params():
 	if len(sys.argv) < 5:
@@ -19,43 +25,46 @@ def check_params():
 	return
 
 def load_params():
-	global bases, ships, ore, energy, bmat
-	ore = int(sys.argv[1])
-	energy = int(sys.argv[2])
+	Mud.ore = int(sys.argv[1])
+	Mud.energy = int(sys.argv[2])
 	basestr = str(sys.argv[3])
-	bases = basestr.split("-")
+	Mud.bases = basestr.split("-")
 	shipstr = str(sys.argv[4])
-	ships = shipstr.split("-")
+	Mud.ships = shipstr.split("-")
 
 def print_stats():
-	global bases, ships, ore, energy, bmat
-	print "Bases: " + str(bases)
-	print "Ships: " + str(ships)
-	print "Ore: " + str(ore)
-	print "Bmat: " + str(bmat)
-	print "Energy: " + str(energy)
-	print "Events: " + str(events)
+	print "Bases: " + str(Mud.bases)
+	print "Ships: " + str(Mud.ships)
+	print "Ore: " + str(Mud.ore)
+	print "Bmat: " + str(Mud.bmat)
+	print "Energy: " + str(Mud.energy)
+	print "Events: " + str(Mud.events)
+
+def display_timer():
+	for i in range(0,5):
+		print "."
+		time.sleep(1)
+	print "."
 	
 def get_event():
-	global bases, ships, ore, energy, bmat
-	eve = MyEvent(10)
-	eve.start()
-	events.append(eve)
+	Mud.newEvent(10)
 
 def build():
 	print "Called build"
 
-def process():	
-	global events
+def process():
 	print "Called process"
-	for i, val in enumerate(events):
+	for i, val in enumerate(Mud.events):
 		print(i, val)
 	p = raw_input("What event(##) would you like to process? ")
-	print "Deal with " + str(events[int(p)])
+	sel = int(p)
+	Mud.events[sel].start()
+	display_timer()
+	Mud.ore += Mud.events[sel].process()
+	del Mud.events[sel]
+	print_stats()
 
 def run():
-	global bases, ships, ore, energy, bmat
-	global myInput
 	myInput = raw_input("Dude enter something (go, build, process, show): ")
 	while myInput != 'exit' and myInput != 'bye' and myInput != 'quit':
 		if myInput == 'process' or myInput == 'p':
