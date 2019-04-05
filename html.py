16abb346855c45db731e1a270e4ba54b12c1019f
Mike Miller <mmiller@apache.org>
2019-04-05 17:48:19 -0400

    Clean up

--- html.py
+++ html.py
@@ -1,23 +1,29 @@
 import sys
 from HTMLParser import HTMLParser
 
-class MLStripper(HTMLParser):
+
+class TagStripper(HTMLParser):
     def __init__(self):
+        HTMLParser.__init__(self)
         self.reset()
         self.fed = []
+
     def handle_data(self, d):
         self.fed.append(d)
+
     def get_data(self):
         return ' - '.join(self.fed)
 
+
 def strip_tags(html):
-    s = MLStripper()
+    s = TagStripper()
     s.feed(html)
     return s.get_data()
 
+
 if len(sys.argv) < 2:
-    #fname = 'help.txt'
-    #with open(fname, 'r') as fin:
+    # fname = 'help.txt'
+    # with open(fname, 'r') as fin:
     print 'usage: python html.py <file>'
     sys.exit()
         
@@ -25,5 +31,5 @@ if len(sys.argv) < 2:
 text = str('')
 f = sys.argv[1]
 with open(f, 'r') as fin:
-   text += fin.read()
+    text += fin.read()
 print 'Text = ' + strip_tags(text)
