6cc9827450df29f06fb52eb574ca48d219b206dc
Mike Miller <mmiller@apache.org>
2019-06-25 17:20:06 -0400

    Format MyEvent

--- MyEvent.py
+++ MyEvent.py
@@ -1,29 +1,26 @@
-import string
-import os
-import sys
 import random
 
 
 class MyEvent:
-	event_types = ['Cosmic','Alien','Apocolyptic', 'Discovery']
+	event_types = ['Cosmic', 'Alien', 'Apocolyptic', 'Discovery']
 
 	started = False
 	number = 0
 	name = ''
 
 	def __init__(self, num):
-		self.number = random.randint(1,num)
-		self.name = MyEvent.event_types[random.randint(0,len(MyEvent.event_types)-1)]
+		self.number = random.randint(1, num)
+		self.name = MyEvent.event_types[random.randint(0, len(MyEvent.event_types)-1)]
 		
 	def start(self):
 		started = True
 		print self.name + " event occured with power " + str(self.number)
 
 	def __str__(self):
-    		return self.name +" "+ str(self.number)
+		return self.name + " " + str(self.number)
 
 	def __repr__(self):
-    		return self.__str__()
+		return self.__str__()
 
 	def process(self):
 		return self.number
