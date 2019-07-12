9c24776b334d203f22435622d547692a1af4c02d
Mike Miller <mmiller@apache.org>
2019-07-12 15:25:01 -0400

    Fix spacing in MyEvent

--- MyEvent.py
+++ MyEvent.py
@@ -2,25 +2,33 @@ import random
 
 
 class MyEvent:
-	event_types = ['Cosmic', 'Alien', 'Apocolyptic', 'Discovery']
+    event_types = ['Cosmic', 'Alien', 'Apocolyptic', 'Discovery']
 
-	started = False
-	number = 0
-	name = ''
+    started = False
+    number = 0
+    name = ''
 
-	def __init__(self, num):
-		self.number = random.randint(1, num)
-		self.name = MyEvent.event_types[random.randint(0, len(MyEvent.event_types)-1)]
-		
-	def start(self):
-		started = True
-		print(self.name + " event occured with power " + str(self.number))
+    def __init__(self, num):
+        self.number = random.randint(1, num)
+        self.name = MyEvent.event_types[random.randint(0, len(MyEvent.event_types)-1)]
 
-	def __str__(self):
-		return self.name + " " + str(self.number)
+    @staticmethod
+    def load(num, name):
+        eve = MyEvent(num)
+        eve.started = True
+        eve.number = num
+        eve.name = name
+        return eve
 
-	def __repr__(self):
-		return self.__str__()
+    def start(self):
+        started = True
+        print(self.name + " event occured with power " + str(self.number))
 
-	def process(self):
-		return self.number
+    def __str__(self):
+        return self.name + " " + str(self.number)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def process(self):
+        return self.number
